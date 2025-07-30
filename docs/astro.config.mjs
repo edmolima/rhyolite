12f0902153d78a91ca2658c009027e6cf4d17d85
Edmo Lima <edmo.lima@onefootball.com>
2025-07-30 10:18:25 +0200

    chore: update config pipeline

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,23 +4,26 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  // Set the site and base for correct asset URLs on GitHub Pages
+  site: 'https://edmolima.github.io/rhyolite/',
+  base: '/rhyolite/docs/',
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
